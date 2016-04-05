5c84ea3e4d64269d2a13af8130fa45be48032916
heltonandreazza <helton.prg@gmail.com>
2016-04-05 09:43:25 -0300

    refactored watch key.tab code

--- dist/select.js
+++ dist/select.js
@@ -759,6 +759,9 @@ uis.controller('uiSelectCtrl',
     if (~[KEY.ENTER,KEY.ESC].indexOf(key)){
       e.preventDefault();
       e.stopPropagation();
+    }
+
+    if (~[KEY.TAB].indexOf(key)){
       if(ctrl.items.length <= 0) {
           ctrl.close();
       }
